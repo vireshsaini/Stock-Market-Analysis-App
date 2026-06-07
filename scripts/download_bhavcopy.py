d5a04dab5afe07f94b0ccc0d1bd2bcc980d7ced8
vireshsaini <30646507+vireshsaini@users.noreply.github.com>
2026-06-07 12:30:06 +0530

    Update download_bhavcopy.py
    modified code..eror removed

--- scripts/download_bhavcopy.py
+++ scripts/download_bhavcopy.py
@@ -9,33 +9,23 @@ SAVE_DIR.mkdir(parents=True, exist_ok=True)
 today = datetime.utcnow()
 
 # Skip Saturday/Sunday
-
 if today.weekday() >= 5:
     print("Weekend - Market Closed")
     raise SystemExit(0)
 
 date_str = today.strftime("%d%m%Y")
-
 filename = f"sec_bhavdata_full_{date_str}.csv"
-
 file_path = SAVE_DIR / filename
 
 # Prevent duplicate download
-
 if file_path.exists():
     print(f"{filename} already exists")
     raise SystemExit(0)
 
-url = (
-    f"https://nsearchives.nseindia.com/"
-    f"products/content/{filename}"
-)
+url = f"https://nsearchives.nseindia.com/products/content/{filename}"
 
 headers = {
-    "User-Agent": (
-        "Mozilla/5.0 "
-        "(Windows NT 10.0; Win64; x64)"
-    ),
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
     "Referer": "https://www.nseindia.com/",
     "Accept-Language": "en-US,en;q=0.9",
     "Accept-Encoding": "gzip, deflate, br",
@@ -48,44 +38,24 @@ session.headers.update(headers)
 MAX_RETRIES = 20
 
 for attempt in range(MAX_RETRIES):
-
-    print(
-        f"Attempt {attempt + 1}/{MAX_RETRIES}"
-    )
+    print(f"Attempt {attempt + 1}/{MAX_RETRIES}")
 
     try:
-
-        response = session.get(
-            url,
-            timeout=30
-        )
+        response = session.get(url, timeout=30)
 
         if response.status_code == 200:
+            with open(file_path, "wb") as f:
+                f.write(response.content)
 
-            with open(
-                file_path,
-                "wb"
-            ) as f:
-
-                f.write(
-                    response.content
-                )
-
-            print(
-                f"Downloaded {filename}"
-            )
-
+            print(f"Downloaded {filename}")
             break
 
-        print(
-            f"HTTP {response.status_code}"
-        )
+        print(f"HTTP {response.status_code}")
+
     except Exception as e:
         print(e)
 
     time.sleep(60)
+
 else:
-    raise Exception(
-        "File not available "
-        "after 20 retries."
-    )
+    raise Exception("File not available after 20 retries.")
